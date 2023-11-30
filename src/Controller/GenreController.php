b928fa2cffd47b217d5d84001c92b1c9ca872053
KLEYER <38478356+RRINKIN@users.noreply.github.com>
2023-11-30 21:02:32 +0100

    adding ajouter genre

--- src/Controller/GenreController.php
+++ src/Controller/GenreController.php
@@ -3,8 +3,10 @@
 namespace App\Controller;
 
 use App\Entity\Genre;
+use App\Form\GenreType;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
@@ -17,6 +19,29 @@ class GenreController extends AbstractController
         $genres = $repository->findAll();
         return $this->render('genre/index.html.twig', [
             'genres' => $genres,
+        ]);
+    }
+
+    #[Route('/genre/ajouter', name: 'app_genre_ajouter')]
+    public function ajouter(Request $request, EntityManagerInterface $entityManager): Response
+    {
+        // creates object
+        $genre = new Genre();
+        // creates form
+        $form = $this->createForm(GenreType::class, $genre);
+        // capture request
+        $form->handleRequest($request);
+        if ($form->isSubmitted() && $form->isValid()) {
+            $genre = $form->getData();
+            // Saving in DB
+            $entityManager->persist($genre);
+            $entityManager->flush();
+            // redirect to home page
+            return $this->redirectToRoute('app_genre');
+        }
+
+        return $this->render('chanson/ajouter.html.twig', [
+            'form' => $form,
         ]);
     }
 }
