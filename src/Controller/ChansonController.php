6347712ef0f757c855576c49922eeff3e97cd452
KLEYER <38478356+RRINKIN@users.noreply.github.com>
2023-11-30 19:55:44 +0100

    finalizing form

--- src/Controller/ChansonController.php
+++ src/Controller/ChansonController.php
@@ -3,14 +3,13 @@
 namespace App\Controller;
 
 use App\Entity\Chanson;
-use App\Entity\Genre;
 use App\Form\ChansonType;
+use DateTime;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
-use Symfony\Component\Validator\Constraints\DateTime as ConstraintsDateTime;
 
 class ChansonController extends AbstractController
 {
@@ -28,10 +27,10 @@ class ChansonController extends AbstractController
         // creates object
         $chanson = new Chanson();
         // creates form
-        //$chanson->setDateAjout(DateType::class);
+        $chanson->setDateAjout(new DateTime());
         $form = $this->createForm(ChansonType::class, $chanson);
         // capture request
-        /*$form->handleRequest($request);
+        $form->handleRequest($request);
         if ($form->isSubmitted() && $form->isValid()) {
             $chanson = $form->getData();
             // Saving in DB
@@ -39,7 +38,7 @@ class ChansonController extends AbstractController
             $entityManager->flush();
             // redirect to home page
             return $this->redirectToRoute('app_chansons');
-        }*/
+        }
 
         return $this->render('chanson/ajouter.html.twig', [
             'form' => $form,
--- src/Form/ChansonType.php
+++ src/Form/ChansonType.php
@@ -3,8 +3,10 @@
 namespace App\Form;
 
 use App\Entity\Chanson;
+use App\Entity\Genre;
+use Symfony\Bridge\Doctrine\Form\Type\EntityType;
 use Symfony\Component\Form\AbstractType;
-use Symfony\Component\Form\Extension\Core\Type\DateTimeType;
+use Symfony\Component\Form\Extension\Core\Type\DateType;
 use Symfony\Component\Form\Extension\Core\Type\SubmitType;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
@@ -19,8 +21,11 @@ class ChansonType extends AbstractType
             ->add('Album', TextType::class)
             ->add('paroles', TextType::class)
             ->add('auteur', TextType::class)
-            ->add('dateSortie', DateTimeType::class)
-            ->add('genre', TextType::class)
+            ->add('dateSortie', DateType::class)
+            ->add('genre', EntityType::class, [
+                'class' => Genre::class,
+                'choice_label' => 'nom',
+            ])
             ->add('Sauver', SubmitType::class, ['label' => 'Sauver'])
         ;
     }
